cda5b97f3175f59b582351178b148fdfd222c616
irvinghu07 <rhu5@dons.usfca.edu>
2018-12-14 00:41:16 -0800

    replacing POJO with  JSONObject during amqp

--- src/main/java/com/irving/rabbitmq/producer/OrderSender.java
+++ src/main/java/com/irving/rabbitmq/producer/OrderSender.java
@@ -1,6 +1,8 @@
 package com.irving.rabbitmq.producer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.irving.rabbitmq.entity.Order;
+import org.json.JSONObject;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +21,17 @@ public class OrderSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     public void send(Order order) throws Exception {
         CorrelationData correlationData = new CorrelationData();
+
         correlationData.setId(order.getMessageId());
         rabbitTemplate.convertAndSend("order-exchange", //交换机exchange(在控制台创建的交换机名)
                 "order.abcd",   //routingKey(在控制台绑定交换机exchange
                 // 和队列queue的时候， 建立的路由规则{order.*匹配一条;order.#匹配任意})
-                order,  //消息体内容
+        new JSONObject(order),  //消息体内容
                 correlationData);   //消息唯一ID
     }
 }
